a7295e8cb9a7a2b55783e08d20ea01644acb5eff
244686710 <244686710@qq.com>
2018-04-17 18:07:40 +0800

    webpack配置

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './app.vue'
+import '../src/assets/style/test.css'
+import '../src/assets/style/test-stylus.styl'
+const root = document.createElement('div')
+document.body.appendChild(root)
+new Vue({
+    render: (h) => h(App)
+}).$mount(root)
